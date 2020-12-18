2b270fa50e46654465ae47244d595440c792bcc7
jlassi wael <jlassiwael@gmail.com>
2020-12-18 10:24:22 +0100

    fix1

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');app.use(express.static("./dist/vtc-comparator-front"));
+const path = require('path');
+app.use(express.static("./dist/vtc-comparator-front"));
 
 app.get("/*", function(req, res) {
   res.sendFile("index.html", {root: 'dist/vtc-comparator-front/'}

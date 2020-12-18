24af8f4ebd6e288fe096496929f2ab3d4ed7602e
jlassi wael <jlassiwael@gmail.com>
2020-12-18 10:45:16 +0100

    fix1

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-
 const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static("./dist/vtc-comparator-front"));
 
-app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/vtc-comparator-front/index.html');
-  console.log(" Fetching from.." + fullPath);
-  res.sendFile(fullPath);
-})
+const app = express();
+
+app.use(express.static('./dist/vtc-comparator-front'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/vtc-comparator-front' }
+  );
+});
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
